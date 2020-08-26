a2c00b26ac11713a760191aef50834afc9d40a52
Leonardo Mendonca <leonardofsm@outlook.com>
2020-08-25 21:55:28 -0300

    remove class Multiplicacao

--- src/PrimeiroProjetoJava/PrimeiroProjeto.java
+++ src/PrimeiroProjetoJava/PrimeiroProjeto.java
@@ -7,7 +7,7 @@ public class PrimeiroProjeto {
     public static void main(String[] args) {
         // TODO code application logic here
         double num1, num2, resp=0;
-        String aux; 
+        String aux =""; 
         
         aux = JOptionPane.showInputDialog(null, "Digite o primeiro número:");
         num1 = Double.parseDouble(aux);
