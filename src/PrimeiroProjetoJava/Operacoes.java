78e2d00cc251fb729530e0e12a74f08510b25495
Leonardo Mendonca <leonardofsm@outlook.com>
2020-09-01 19:15:41 -0300

    classe de adicao

--- src/PrimeiroProjetoJava/Operacoes.java
+++ src/PrimeiroProjetoJava/Operacoes.java
@@ -4,8 +4,11 @@ package PrimeiroProjetoJava;
 public class Operacoes {
     
             public static Double multiplicacao (double var1, double var2) {
-       return (var1 * var2);
-    }
-    
+                 return (var1 * var2);
+            }
+            
+             public Double adicao (double var1, double var2) {
+                return (var1 + var2);
+            }
     
 }
